9ca6c400c12da920a8e9540babb541329f5820a5
stephanec <stephanec@washingmachine>
2014-04-20 04:19:01 +0200

    red_john_is_back

--- red_john_is_back.go
+++ red_john_is_back.go
@@ -9,14 +9,14 @@ var prime_numbers = []int{2, 3, 5, 7}
 
 // total number of ways in which the bricks can be arranged on the wall
 // of size 4xnumber with bricks of size 4x1 and 1x4.
-var bricks [41]int
+var bricks [42]int
 
 func gen_bricks() {
     var i int
     for ; i < 5; i++ {
         bricks[i] = 1
     }
-    for ; i < 41; i++ {
+    for ; i < 42; i++ {
         bricks[i] = bricks[i - 1] + bricks[i - 4]
     }
 }
