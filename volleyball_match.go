99f2063da47d8b0fbbcd044c1cf7af2985ca057d
stephanec <stephanec@washingmachine>
2014-04-23 05:21:33 +0200

    volleyball_match

--- volleyball_match.go
+++ volleyball_match.go
@@ -2,41 +2,66 @@ package main
 
 import "fmt"
 import "math"
+import "math/big"
 
 func main() {
-    var a, b float64
-    fmt.Scanf("%f", &a)
-    fmt.Scanf("%f", &b)
+    var a, b int64
+    fmt.Scanf("%d", &a)
+    fmt.Scanf("%d", &b)
 
-    var c = a + b - 1
-    var d, e = math.Min(a, b), math.Max(a, b)
+    var d, e int64
+    // var d, e = math.Min(a, b), math.Max(a, b)
+    if a < b {
+        d = a
+        e = b
+    } else {
+        d = b
+        e = a
+    }
 
     if e < 25 {
         fmt.Println(0)
 
-    } else if e == 25 && d == 24 {
+    } else if e == 25 && d >= 24 {
         fmt.Println(0)
 
     } else if e > 25 && e - d != 2 {
         fmt.Println(0)
 
     } else {
-        var f = math.Pow10(9) + 7.
-        var g = factorial(c) / (factorial(d) * factorial(c-d))
-
-        if g > f {
-            fmt.Println(uint(math.Mod(g, f)))
+        // var g = factorial(c) / (factorial(d) * factorial(c-d))
+        // http://fr.wikipedia.org/wiki/Loi_binomiale
+        var c = a + b - 1
+        if e > 25 {
+            c -= 1
+        }
+        var f = big.NewInt(int64(math.Pow10(9) + 7))
+        var z3 = big.NewInt(1).Binomial(c, d)
+        if z3.Cmp(f) == 1 {
+            fmt.Println(z3.Mod(z3, f))
         } else {
-            fmt.Println(uint(g))
+            fmt.Println(z3.Abs(z3))
         }
     }
 }
 
-func factorial(a float64) float64 {
-    var b float64 = 1.
-    for ; a > 1; a-- {
-        b *= a
-    }
-    return b
-}
+
+// func factorial(n *big.Int) *big.Int {
+    // var result = big.NewInt(1)
+    // var one = big.NewInt(1)
+    // for n.Cmp(one) == 1 {
+        // result.Mul(result, n)
+        // n.Sub(n, one)
+    // }
+    // return result
+// }
+
+
+// func factorial(a float64) float64 {
+    // var b float64 = 1.
+    // for ; a > 1; a-- {
+        // b *= a
+    // }
+    // return b
+// }
 
--- volleyball_match.py
+++ volleyball_match.py
@@ -3,19 +3,20 @@ import math
 a = long(raw_input())
 b = long(raw_input())
 
-c = a + b - 1
 d = min(a, b)
 e = max(a, b)
 
-# print a, b, c, d, e
 
 if e < 25:
     print 0
-elif e == 25 and d == 24:
+elif e == 25 and d >= 24:
     print 0
 elif e > 25 and e - d != 2:
     print 0
 else:
+    c = a + b - 1
+    if e > 25:
+        c -= 1
     f = 10**9 + 7
     g = long(math.factorial(c) / (math.factorial(d) * math.factorial(c-d)))
     if g > f:
@@ -25,9 +26,8 @@ else:
 
 
 
-print
-# combinations
-print math.factorial(e) / (math.factorial(d) * math.factorial(e-d))
-
-# permutations
-print math.factorial(e) / math.factorial(e-d)
+## combinations
+# print math.factorial(e) / (math.factorial(d) * math.factorial(e-d))
+#
+## permutations
+# print math.factorial(e) / math.factorial(e-d)
