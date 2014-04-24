edfc039508e476c2fd5e12a87d5dd00da8795cf0
stephanec <stephanec@washingmachine>
2014-04-24 06:05:52 +0200

    p_sequences

---
+++ p_sequences.go
@@ -0,0 +1,101 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+    var i, n, p int
+    fmt.Scanf("%d", &n)
+    fmt.Scanf("%d", &p)
+    // debug(n , p)
+
+    var elements = Range(1, p + 1)
+    // debug("elements", elements)
+
+    var productn = make([][]int, n)
+    for i = 0; i < n; i++ {
+        productn[i] = elements
+    }
+    // debug("productn", productn)
+
+    var products = Product(p, productn...)
+    // for i, product := range products {
+        // if i % p == 0 {
+            // fmt.Fprintln(os.Stderr)
+        // }
+        // fmt.Fprint(os.Stderr, product)
+    // }
+    // fmt.Fprintln(os.Stderr)
+    // debug("products", len(products))
+    fmt.Println(len(products))
+    // 2 2: 3
+    // 3 4: 4*4+6=22
+    // 3 3: 3*3+2=11
+    // 3 5: 5*5+7=32
+    // 4 3: 3*5+6=21
+    // 2 5: 10
+}
+
+func debug(args ...interface{}) {
+    fmt.Fprintln(os.Stderr, args)
+}
+
+func Product(p int, args ...[]int) [][]int {
+    pools := args
+    npools := len(pools)
+    indices := make([]int, npools)
+
+    result := make([]int, npools)
+    for i := range result {
+        if len(pools[i]) == 0 {
+            return nil
+        }
+        result[i] = pools[i][0]
+    }
+
+    results := [][]int{result}
+
+    for {
+        i := npools - 1
+        for ; i >= 0; i -= 1 {
+            pool := pools[i]
+            indices[i] += 1
+
+            if indices[i] == len(pool) {
+                // debug("i:", i, "indices:", indices, result, ";")
+                indices[i] = 0
+                result[i] = pool[0]
+
+            } else {
+                result[i] = pool[indices[i]]
+                // debug("i:", i, "indices:", indices, result)
+
+                if i > 0 && result[i] * result[i-1] > p {
+                    indices[i] = 0
+                    result[i] = pool[0]
+                    continue
+                } else {
+                    break
+                }
+            }
+        }
+
+        if i < 0 {
+            return results
+        }
+
+        newresult := make([]int, npools)
+        copy(newresult, result)
+        results = append(results, newresult)
+    }
+
+    return nil
+}
+
+func Range(start, stop int) []int {
+    var list = make([]int, stop-start)
+    for i := 0; i < stop-start; i++ {
+        list[i] = start + i
+    }
+    return list
+}
